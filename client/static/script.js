4f6351e9c71247c62bda7b73a1919214fe7898a0
uberfig <aidan.gifford04@gmail.com>
2024-02-24 19:40:01 -0500

    Update script.js

--- client/static/script.js
+++ client/static/script.js
@@ -457,19 +457,9 @@ function notifyMe() {
 		console.log("This browser does not support desktop notification");
 	} else if (Notification.permission === "granted") {
 		// Check whether notification permissions have already been granted;
-		// if so, create a notification
-		// const notification = new Notification("Hi there!");
-		// …
 	} else if (Notification.permission !== "denied") {
 		// We need to ask the user for permission
 		Notification.requestPermission();
-		// .then((permission) => {
-		// 	// If the user accepts, let's create a notification
-		// 	if (permission === "granted") {
-		// 		const notification = new Notification("Hi there!");
-		// 		// …
-		// 	}
-		// });
 	}
 }
 
